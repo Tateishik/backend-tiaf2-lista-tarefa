90baac9d2edba98bdf16797924d12d6ebac9516b
Tateishik <kaua.tateishi@hotmail.com>
2024-10-17 09:14:10 -0300

    Cadastro de Usuários e Autenticação com JWT e Bcrypt

--- db/database.js
+++ db/database.js
@@ -3,7 +3,18 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('lista-tarefas.db');
 
 db.serialize(() => {
+    // Criação da tabela de tarefas
     db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
+
+    // Criação da tabela de usuários
+    db.run(`
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY,
+            username TEXT UNIQUE,
+            password TEXT,
+            role TEXT
+        )
+    `);
 });
 
 module.exports = db;
